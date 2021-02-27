a2c89cde1e2ce84291efe2c13f90de8f428652b9
Zehadh <zehadh45@gmail.com>
2021-02-27 20:04:06 +0600

    ultra-nation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries,setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    .catch(error => console.log(error));
+  },[])
   return (
     <div className="App">
+      <h1>{countries.length}</h1>
+      <ul>
+        {
+          countries.map(country => <li>{country.name}</li>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
